81acb79e4d44ba82af7d6bba965aeff0fca06251
SeukWon Kang <kasworld@gmail.com>
2025-02-11 13:47:13 +0900

    bullet explode scene

--- main.gd
+++ main.gd
@@ -1,10 +1,19 @@
 extends Node2D
 
 var invader1_scene = preload("res://invader_1.tscn")
+var invader1bullet_scene = preload("res://invader_1_bullet.tscn")
 var invader2_scene = preload("res://invader_2.tscn")
+var invader2bullet_scene = preload("res://invader_2_bullet.tscn")
 var invader3_scene = preload("res://invader_3.tscn")
+var invader3bullet_scene = preload("res://invader_3_bullet.tscn")
+var explode_scene = preload("res://explode.tscn")
+
 var fighter_scene = preload("res://fighter.tscn")
+var fighter_bullet_scene = preload("res://fighter_bullet.tscn")
+
 var ufo_scene = preload("res://ufo.tscn")
+var ufo_bullet_scene = preload("res://ufo_bullet.tscn")
+var ufo_explode_scene = preload("res://ufo_explode.tscn")
 
 func _ready() -> void:
 	pass
