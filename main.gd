609b0e671aff734672b0d4f0273f7879e73143d6
SeukWon Kang <kasworld@gmail.com>
2025-02-11 13:37:31 +0900

    add scene

--- main.gd
+++ main.gd
@@ -1,1 +1,10 @@
 extends Node2D
+
+var invader1_scene = preload("res://invader_1.tscn")
+var invader2_scene = preload("res://invader_2.tscn")
+var invader3_scene = preload("res://invader_3.tscn")
+var fighter_scene = preload("res://fighter.tscn")
+var ufo_scene = preload("res://ufo.tscn")
+
+func _ready() -> void:
+	pass
