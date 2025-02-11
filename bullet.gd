db39bbe5c0f926b330667443a2302e67610ac1f9
SeukWon Kang <kasworld@gmail.com>
2025-02-11 20:16:01 +0900

    merge bullet

---
+++ bullet.gd
@@ -0,0 +1,26 @@
+extends Node2D
+class_name Bullet
+
+enum Type {Invader1,Invader2,Invader3,UFO,Fighter}
+
+var bullet_type : Type
+
+func set_type(t : Type) -> Bullet:
+	bullet_type = t
+	match bullet_type:
+		Type.Invader1:
+			$Sprite2D.texture = preload("res://assets/invader1_bullet.png")
+		Type.Invader2:
+			$Sprite2D.texture = preload("res://assets/invader2_bullet.png")
+		Type.Invader3:
+			$Sprite2D.texture = preload("res://assets/invader3_bullet.png")
+		Type.UFO:
+			$Sprite2D.texture = preload("res://assets/ufo_bullet.png")
+		Type.Fighter:
+			$Sprite2D.texture = preload("res://assets/fighter_bullet.png")
+		_ :
+			print_debug("invalid bullet type ", bullet_type)
+	return self
+
+func next_frame() -> void:
+	$Sprite2D.flip_h = not $Sprite2D.flip_h
--- main.gd
+++ main.gd
@@ -1,20 +1,18 @@
 extends Node2D
 
 var invader_scene = preload("res://invader.tscn")
-var invader_bullet_scene = preload("res://invader_bullet.tscn")
+var bullet_scene = preload("res://bullet.tscn")
 var invader_explod_scene = preload("res://invader_explode.tscn")
 
 var fighter_scene = preload("res://fighter.tscn")
-var fighter_bullet_scene = preload("res://fighter_bullet.tscn")
 var fighter_explode_scene = preload("res://fighter_explode.tscn")
 
 var ufo_scene = preload("res://ufo.tscn")
-var ufo_bullet_scene = preload("res://ufo_bullet.tscn")
 var ufo_explode_scene = preload("res://ufo_explode.tscn")
 
 
 var inv1 : Invader
-var bul1 : InvaderBullet
+var bul1 : Bullet
 var vp_size :Vector2
 func _ready() -> void:
 	vp_size = get_viewport_rect().size
@@ -23,7 +21,7 @@ func _ready() -> void:
 	add_child(inv1)
 	inv1.position = vp_size *0.3
 
-	bul1 = invader_bullet_scene.instantiate().set_type(Invader.Type.Invader1)
+	bul1 = bullet_scene.instantiate().set_type(Bullet.Type.Invader1)
 	add_child(bul1)
 	bul1.position = vp_size * 0.6
 
