2f819d00b75c6c1c0e35baf898e1e8743e64f590
SeukWon Kang <kasworld@gmail.com>
2025-02-12 14:03:03 +0900

    add game object to list

--- main.gd
+++ main.gd
@@ -7,8 +7,9 @@ var fighter_scene = preload("res://fighter.tscn")
 var ufo_scene = preload("res://ufo.tscn")
 
 var vp_size :Vector2
-var obj_list := []
 var invader_list := []
+var bullet_list := []
+var explode_list := []
 var ufo :UFO
 var fighter :Fighter
 
@@ -37,46 +38,49 @@ func _ready() -> void:
 			invader_list.append(o)
 			o.position = Vector2( (i+1) * inv_w, inv_h * (j+5) )
 
-	var o = ufo_scene.instantiate()
-	add_child(o)
-	obj_list.append(o)
-	o.position = Vector2( (4) * inv_w, inv_h * (1) )
-	ufo = o
+	ufo = ufo_scene.instantiate()
+	add_child(ufo)
+	ufo.position = Vector2( (4) * inv_w, inv_h * (1) )
 
-	o = fighter_scene.instantiate()
-	add_child(o)
-	obj_list.append(o)
-	o.position = Vector2( (5) * inv_w, inv_h * (10) )
-	fighter = o
+	fighter = fighter_scene.instantiate()
+	add_child(fighter)
+	fighter.position = Vector2( (5) * inv_w, inv_h * (10) )
 
-	add_obj(bullet_scene.instantiate().set_type(Bullet.Type.Invader1) )
-	add_obj(bullet_scene.instantiate().set_type(Bullet.Type.Invader2) )
-	add_obj(bullet_scene.instantiate().set_type(Bullet.Type.Invader3) )
-	add_obj(bullet_scene.instantiate().set_type(Bullet.Type.UFO) )
-	add_obj(bullet_scene.instantiate().set_type(Bullet.Type.Fighter) )
+	bullet_list.append(bullet_scene.instantiate().set_type(Bullet.Type.Invader1) )
+	bullet_list.append(bullet_scene.instantiate().set_type(Bullet.Type.Invader2) )
+	bullet_list.append(bullet_scene.instantiate().set_type(Bullet.Type.Invader3) )
+	bullet_list.append(bullet_scene.instantiate().set_type(Bullet.Type.UFO) )
+	bullet_list.append(bullet_scene.instantiate().set_type(Bullet.Type.Fighter) )
+	for o in bullet_list:
+		add_child(o)
+		o.position = Vector2( randf_range(0,vp_size.x), randf_range(0,vp_size.y))
 
-	add_obj(explode_scene.instantiate().set_type(Explode.Type.Invader) )
-	add_obj(explode_scene.instantiate().set_type(Explode.Type.UFO) )
-	add_obj(explode_scene.instantiate().set_type(Explode.Type.Fighter) )
 
-func add_obj(o ) -> void:
-	add_child(o)
-	o.position = Vector2( randf_range(0,vp_size.x), randf_range(0,vp_size.y))
-	obj_list.append(o)
+	explode_list.append(explode_scene.instantiate().set_type(Explode.Type.Invader) )
+	explode_list.append(explode_scene.instantiate().set_type(Explode.Type.UFO) )
+	explode_list.append(explode_scene.instantiate().set_type(Explode.Type.Fighter) )
+	for o in explode_list:
+		add_child(o)
+		o.position = Vector2( randf_range(0,vp_size.x), randf_range(0,vp_size.y))
 
+var inv_num := 0
 func _process(delta: float) -> void:
 	ufo.position.x += 3
 	if ufo.position.x > vp_size.x:
 		ufo.position.x = 0
 
-func _on_timer_timeout() -> void:
-	for o in obj_list:
-		o.next_frame()
-		o.set_color(NamedColorList.color_list.pick_random()[0])
+	change_frame_color(invader_list[inv_num])
+	inv_num += 1
+	inv_num %= invader_list.size()
 
-	for o in invader_list:
-		o.next_frame()
-		o.set_color(NamedColorList.color_list.pick_random()[0])
+func change_frame_color(o) -> void:
+	o.next_frame()
+	o.set_color(NamedColorList.color_list.pick_random()[0])
+
+func _on_timer_timeout() -> void:
+	change_frame_color(ufo)
+	change_frame_color(fighter)
+
 
 # esc to exit
 func _unhandled_input(event: InputEvent) -> void:
